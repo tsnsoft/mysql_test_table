1cafb7e2b0836d388f4eb9eae45f66a9d3ea6b54
Talipov S.N <proffix4@gmail.com>
2020-08-29 23:17:12 +0600

    Add files via upload

---
+++ my_books.sql
@@ -0,0 +1,10 @@
+CREATE TABLE `my_books` (
+  `id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `author` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+INSERT INTO my_books (id, `name`, author) VALUES (1, 'Effective Java', 'Joshua Bloch');
+INSERT INTO my_books (id, `name`, author) VALUES (2, 'Java Concurrency in Practice', 'Brian Goetz');
+INSERT INTO my_books (id, `name`, author) VALUES (3, 'Современное визуальное программирование', 'Талипов С.Н.');
